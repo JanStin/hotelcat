5bc0571079418a33aa1c18409718e63192717df1
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-03-05 19:01:53 +0300

    Finished slide-show without adaptive.

--- src/components/items/slide-show/slide-show.js
+++ src/components/items/slide-show/slide-show.js
@@ -1,17 +1,17 @@
 $(document).ready(() => {
-    $('.slide-show__additional').click(function() {
-        // TODO: нельзя повторно сделать изначальный элемент классом.
+    $('.slide-show__js').click(function() {
+        if($(this).hasClass('slide-show__main')) {
+            return;
+        }
+
         let additionalImageClass = $(this).attr('class');
         let additionalImage = $(this);
         let mainImage = $(this).parent().find('.slide-show__main');
 
         mainImage.removeClass();
         additionalImage.removeClass();
-
-        // mainImage.addClass(additionalImageClass);        
+    
         mainImage.addClass(additionalImageClass);        
         additionalImage.addClass('slide-show__main');
-        console.log(mainImage.parent());
-        // console.log($(mainImage));
     });
 });
