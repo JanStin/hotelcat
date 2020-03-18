e25d8f99fe779a65f0e0dd9e8a41dba58997e10d
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2020-03-18 21:10:35 +0300

    Any fix

--- src/components/items/number/number.js
+++ src/components/items/number/number.js
@@ -24,4 +24,37 @@ $(document).ready(function() {
             }
         }
     });
+
+    setInterval(function() {
+        if (!$(".number__dot.active").length){
+            return;
+        }
+        
+        let dot = $('.number__dot.active');
+        let dots = dot.parent().find('.number__dot');
+        let parent = dot.parents();
+        let slides = parent.find('.number__photo');
+
+        let active = dot.data().value + 1;
+        if (active == slides.length) {
+            active = 0;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            let slide = $(slides[i]);
+            slide.addClass('hidden');
+            if(slide.data().value == active) {
+                slide.removeClass('hidden');
+            }
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dot = $(dots[i]);
+            if(dot.data().value != active) {
+                dot.removeClass('active');
+            } else {
+                dot.addClass('active');
+            }
+        }
+    }, 10000);
 }); 
